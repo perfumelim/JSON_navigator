ea58ff094854d839027daa5918eafa526759cb11
perfumelim <perfumelim@gmail.com>
2022-04-10 23:55:14 +0900

    ✨  feat: file upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import React, { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [fileData, setFileData] = useState({
+    file: "",
+    fileContent: "{}",
+  });
+  const [objKeys, setObjKeys] = useState([]);
+
+  const onUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      setFileData({ fileName: file.name, fileContent: reader.result });
+    };
+    reader.onerror = () => {
+      console.log("file error", reader.error);
+    };
+  };
+
+  useEffect(() => {
+    const intermediateFormat = Object.values(fileData.fileContent).join("");
+    console.log("should be valid", intermediateFormat);
+    setObjKeys(Object.keys(JSON.parse(intermediateFormat)));
+
+    console.log("key", objKeys);
+    console.log("file Data", fileData.fileContent);
+  }, [fileData]);
+
+  return (
+    <div className="App">
+      <form id="upload">
+        <label htmlFor="file">File to upload</label>
+        <input type="file" id="file" accept=".json" onChange={onUpload} />
+        <button>Upload</button>
+      </form>
+      <p>{objKeys}</p>
+    </div>
+  );
 }
 
 export default App;

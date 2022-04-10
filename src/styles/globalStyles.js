71bd72b9e09a471ae18b9e19455683e96525f467
perfumelim <perfumelim@gmail.com>
2022-04-11 00:16:56 +0900

    💄 Add styled-components

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    html,
+    body,
+    #root {
+    height: 100%;
+    }
+    * {
+    box-sizing: border-box;
+    }
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+    button {
+        background: inherit;
+        border: none;
+        box-shadow:none;
+    }
+    ol,ul,li {
+        list-style: none;
+    }
+`;
+
+export default GlobalStyles;
